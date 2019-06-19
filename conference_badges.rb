7c52acfdd152915a91ddfc70456e110814198dce
Rashard Pleasant <rashard.pleasant@gmail.com>
2019-06-19 01:54:39 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,4 +7,8 @@ def badge_maker(name)
    attendees.each do |attendee|
  end
   
- 
+ def assign_rooms(attendees)
+   r = []
+   attendees.each_with_index do |name, room, attendees|
+   "Hello, #(name) You'll be assigned to room #(room_number)"
+ end
